1ab8ae51fb7afe1211ba7be572d0ca28cf6c0863
Shiragu <pipboy2009@yandex.ru>
2021-06-27 15:31:35 +0300

    redux for auth section

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { apiKey } from "../../axios/apiKey";
+import { AUTH_LOGOUT, AUTH_SUCCESS } from "./actionTypes";
 
 export function auth(email, password, isLogin) {
   return async (dispatch) => {
@@ -15,5 +16,41 @@ export function auth(email, password, isLogin) {
     }
 
     const response = await axios.post(url, authData);
+    const data = response.data;
+
+    const expirationDate = new Date(
+      new Date().getTime() + data.expiresIn * 1000
+    );
+
+    localStorage.setItem("token", data.idToken);
+    localStorage.setItem("userId", data.localId);
+    localStorage.setItem("expirationDate", expirationDate);
+
+    dispatch(authSuccess(data.idToken));
+    dispatch(autoLogout(data.ixpiresIn));
+  };
+}
+
+export function autoLogout(time) {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, time * 1000);
+  };
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("expirationDate");
+  return {
+    type: AUTH_LOGOUT,
+  };
+}
+
+export function authSuccess(token) {
+  return {
+    type: AUTH_SUCCESS,
+    token,
   };
 }
